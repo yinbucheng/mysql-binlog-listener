aef68c9975c8ddd730c2384b8d1c8958b27bbff4
yinchong <yin.chong@intellif.com>
2019-08-01 16:59:22 +0800

    修改映射需要所有字段问题

--- src/main/java/cn/bucheng/mysql/utils/BinLogUtils.java
+++ src/main/java/cn/bucheng/mysql/utils/BinLogUtils.java
@@ -4,8 +4,12 @@ package cn.bucheng.mysql.utils;
 import org.apache.commons.beanutils.BeanUtils;
 
 import java.io.Serializable;
+import java.lang.reflect.Field;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.Map;
 import java.util.Set;
+import java.util.concurrent.ConcurrentHashMap;
 
 /**
  * @author ：yinchong
@@ -20,19 +24,45 @@ public class BinLogUtils {
         return dbName + "-" + tableName;
     }
 
+    private static Map<String, List<String>> clazzFields = new ConcurrentHashMap<>();
+
 
     public static <T> T decode(Class<T> clazz, Map<String, Serializable> msg) {
         try {
             T cls = clazz.newInstance();
-            Set<Map.Entry<String, Serializable>> entrySet = msg.entrySet();
-            for (Map.Entry<String, Serializable> entry : entrySet) {
-                BeanUtils.setProperty(cls, entry.getKey(), entry.getValue());
+            List<String> properties = getPropertiesFromClass(clazz);
+            if (properties != null) {
+                for (String property : properties) {
+                    BeanUtils.setProperty(cls, property, msg.get(property));
+                }
             }
             return cls;
         } catch (Exception e) {
             e.printStackTrace();
         }
         return null;
+    }
+
+    private static List<String> getPropertiesFromClass(Class clazz) {
+        List<String> result = clazzFields.get(clazz.getName());
+        if (result != null) {
+            return result;
+        }
+        synchronized (clazz) {
+            result = clazzFields.get(clazz.getName());
+            if (result == null) {
+                Field[] fields = clazz.getDeclaredFields();
+                if (fields != null) {
+                    result = new LinkedList<>();
+                    for (Field field : fields) {
+                        field.setAccessible(true);
+                        result.add(field.getName());
+                    }
+                }
+                clazzFields.put(clazz.getName(), result);
+            }
+        }
+        return result;
     }
 
 
